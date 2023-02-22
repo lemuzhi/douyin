b4005f9287bde96ae4ea0f20528fbad19f2f260b
raidenneox <raidennrk@outlook.com>
2023-02-22 23:51:45 +0800

    bug fix

--- internal/model/response/common.go
+++ internal/model/response/common.go
@@ -41,8 +41,3 @@ type MessageSendEvent struct {
 	ToUserId   int64  `json:"to_user_id,omitempty"`
 	MsgContent string `json:"msg_content,omitempty"`
 }
-
-type MessagePushEvent struct {
-	FromUserId int64  `json:"user_id,omitempty"`
-	MsgContent string `json:"msg_content,omitempty"`
-}
