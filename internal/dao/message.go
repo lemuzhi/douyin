095c56469694bf86bbb92f92eac08f8154a5e6ef
lemuzhi <1163648924@qq.com>
2023-02-24 14:59:58 +0800

    fix: message list bug

--- internal/dao/message.go
+++ internal/dao/message.go
@@ -42,7 +42,7 @@ func (dao *Dao) SendMessage(userId uint, toUserId uint, content string) error {
 func (dao *Dao) GetMessagebyIdAndTime(formUserId uint, toUserId uint, PreMsgTime int64) (message []model.Message, err error) {
 	if PreMsgTime == 0 {
 		//未传入上次最新的消息时间，从最早发布的消息开始获取
-		err = dao.db.Where("from_user_id = ? AND to_user_id = ?", toUserId, formUserId).Order("create_time").Last(&message).Error
+		err = dao.db.Debug().Where("(from_user_id = ? AND to_user_id = ?) OR (to_user_id = ? AND from_user_id = ?)", toUserId, formUserId, toUserId, formUserId).Order("create_time").Find(&message).Error
 		return
 	}
 	err = dao.db.Where("from_user_id = ? AND to_user_id = ? AND create_time > ?", toUserId, formUserId, PreMsgTime).Find(&message).Error
